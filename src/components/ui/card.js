6f530f022574243c1f9a964cb68b2ffa8fe43af3
AruneshDev <aruneshkumarlal@gmail.com>
2025-04-06 12:48:11 -0400

    updated ui

---
+++ src/components/ui/card.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export function Card({ children, className }) {
+    return (
+        <div className={`p-4 shadow-md rounded-lg bg-white ${className}`}>
+            {children}
+        </div>
+    );
+}
+
+export function CardContent({ children }) {
+    return (
+        <div className="p-2">
+            {children}
+        </div>
+    );
+}
